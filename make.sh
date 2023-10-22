5508d34270d7c17dc65e91c8a681b49c0ed5b589
poolpog <poolpog@gmail.com>
2023-10-22 15:00:34 -0400

    example run script

---
+++ make.sh
@@ -0,0 +1,39 @@
+set -x
+
+SRCFILE=${1}
+WORKDIR=$(pwd)
+TEMPDIR=$( mktemp -d ${WORKDIR}/homebrewery.XXXXXXXX )
+
+function usage() {
+    echo
+    echo "Usage: $0 <srcfile.md>"
+    exit 1
+}
+
+function cleanup() {
+    rm -rf ${TEMPDIR}
+}
+trap cleanup EXIT
+
+if [[ -z "${SRCFILE}" ]]; then
+    echo
+    echo "Need Markdown source file to make into homebrewery"
+    usage
+fi
+
+pushd ${TEMPDIR}
+git clone git@github.com:naturalcrit/homebrewery.git hb
+cd hb
+git remote add G-Ambatte git@github.com:G-Ambatte/homebrewery.git
+git fetch G-Ambatte
+git checkout remotes/G-Ambatte/experimentalCommandLineBrewProcess
+docker-compose build
+docker run -it --rm \
+    -v ${WORKDIR}:/usr/src/app/workdir homebrewery \
+        node cli/process.js \
+        --input /usr/src/app/workdir/${SRCFILE} \
+        --output /usr/src/app/workdir/x.html \
+        --renderer v3 \
+        --overwrite
+
+
